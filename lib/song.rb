5479d16d948e078e0fd4bca4f8e3b30f40fcbecb
Karim Alomari <karim.alomari@gmail.com>
2020-08-20 16:19:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,13 @@
 class Song 
   attr_accessor :name, :artist, :genre
-  
   @@count = 0
   
-  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@artists << @artist
+    @@genres << @genre
+  end
 end
